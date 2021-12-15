cb80119f58627a58f0c42f90ecc530d130153611
JaackoobNo1 <Jock@op.pl>
2021-12-15 20:27:06 +0100

    Display v1.1
    Zmiana z wyświetlania wartości na wejściu analogowym A1 na samo wyświetlanie zmiennej "wynik".

--- opto_v1/display.c
+++ opto_v1/display.c
@@ -2,20 +2,18 @@
 
 LiquidCrystal lcd(7,8,9,10,11,12);
 
-int wynik = A1;
+int wynik;
 
 void setup()
 {
-lcd.begin(16, 1);
+lcd.begin(16, 2);
 lcd.clear();
-
-pinMode(wynik, INPUT);
 }
 
 void loop()
 {
 lcd.setCursor(0,0); 
 lcd.print("Obroty/minute: "); 
-lcd.print(analogRead(wynik));
+lcd.print(wynik);
 delay(500); 
 }
