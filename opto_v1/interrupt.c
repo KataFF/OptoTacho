0b9cabfe119b52f6c7d0c7ca8788c870b4fcc29f
jestem-andrew <jedrekmalolepszy@gmail.com>
2021-12-15 22:54:03 +0100

    przerwanie
    pierszy prototyp

--- opto_v1/interrupt.c
+++ opto_v1/interrupt.c
@@ -1,1 +1,20 @@
 
+
+ISR(TIMER1_OVF_vect) {
+ //kiedy cos kreci sie za wolno, i mamy timer overflow
+  tooslow = 1;
+}
+
+
+void przerwanie() {
+  
+  for(int i=0; i<5;i++){}//czekamy 5 cykli na ustabilizowanie wejścia
+  if(analogRead(input_pin) > Vref) //analogRead zwraca wartości od 0 do 1024(5V) -> wirtualny przerzutnik schmitta
+  {
+    //TODO sprawdzic jakie napiecie mamy na input pinie (Vref)
+    rpmtime = TCNT1; //wartość z licznika
+    TCNT1 = 0; //zerowanie flagi
+    tooslow = 0;
+  }
+  
+}
