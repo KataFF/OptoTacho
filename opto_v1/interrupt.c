fcdc353a7a0ff5a312599f92e5d71d9d3b04c2e8
jestem-andrew <jedrekmalolepszy@gmail.com>
2021-12-19 17:44:05 +0100

    usprawnienie przerwania
    Okazuje się że to przerwanie średnio działa z włączonym timerem. Póki co zrobiłem tak żeby samo wychwytywało i wyświetlało gdy mamy określone napięcie na wyjściu, bez timera.

--- opto_v1/interrupt.c
+++ opto_v1/interrupt.c
@@ -1,6 +1,6 @@
 #include "interrupt.h"
 #define interruptPin 2
-#define Vref 900 // 4,5V/5V * 1024 = 
+#define Vref 180 // 4,5V/5V * 1024 = 
 
 _Bool tooslow = 1;
 
@@ -10,18 +10,24 @@ ISR(TIMER1_OVF_vect) {
 }
 
 void przerwanie() {
-  for(int i=0; i<5;i++){}//czekamy 5 cykli na ustabilizowanie wejścia
-  if(analogRead(interruptPin) > Vref) //analogRead zwraca wartości od 0 do 1024(5V) -> wirtualny przerzutnik schmitta
-  { //sprawdzamy czy nie było to zakłócenie
+  //for(int i=0; i<5;i++){}//czekamy 5 cykli na ustabilizowanie wejścia
+  //if(analogRead(analog_pin) < Vref) //analogRead zwraca wartości od 0 do 1024(5V) -> wirtualny przerzutnik schmitta
+   //sprawdzamy czy nie było to zakłócenie
     
-    for(int i=0; i<5;i++){}//czekamy 5 cykli na ustabilizowanie wejścia
-    {
-      if(analogRead(interruptPin) > Vref) //analogRead zwraca wartości od 0 do 1024(5V) -> wirtualny przerzutnik schmitta
+    //for(int i=0; i<5;i++){}//czekamy 5 cykli na ustabilizowanie wejścia
+    
+      if(analogRead(analog_pin) < Vref) //analogRead zwraca wartości od 0 do 1024(5V) -> wirtualny przerzutnik schmitta
       //TODO sprawdzic jakie napiecie mamy na input pinie (Vref)
+      {
       rpmtime = TCNT1; //wartość z licznika
       TCNT1 = 0; //zerowanie flagi
-      tooslow = 0;
-    }
-  }
+      Serial.print("x ");
+      Serial.print(j);
+      Serial.print("  ");
+      Serial.print(analogRead(analog_pin));
+      Serial.println();
+      j++;
+      }
+  
   
 }
