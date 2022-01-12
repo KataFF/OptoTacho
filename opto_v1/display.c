cd7e08b82cf95b9a681bc3e9b7b28eab735732e4
jestem-andrew <jedrekmalolepszy@gmail.com>
2022-01-12 22:31:25 +0100

    Zmiana display.c
    Żeby się zgadzało ze sprawkiem
    + usprawnienia

--- opto_v1/display.c
+++ opto_v1/display.c
@@ -1,7 +1,10 @@
-void wyswietl()
+void wyswietlRpm(unsigned int rpm)
 {
-lcd.setCursor(0,0); 
-lcd.write("Obroty/minute: "); 
-lcd.write(obliczone);
-delay(500); 
+lcd.clear();    //czyszczenie ekranu z wszelkich pozostałości
+lcd.setCursor(0,0); //postawienie kursora w lewym gornym rogu LCD 
+lcd.write("RPM: ");  // wyswietlenie 1. linijki tekstu
+lcd.setCursor(0,1); //postawienie kursora w drugiej linijce
+lcd.print(rpm); //wypisz wynik
+delay(500); //delay zeby moc odczytac wynik
+lcd.clear(); //po pół sekundy odśwież
 }
